37c291e911bd76931f80fed61b15331881dd4142
Anton Fomenko <anton.fomenko.val@gmail.com>
2025-03-26 15:08:02 +0000

    Add headers test

--- dot-net/ReliableDownloader.Tests/Tests.cs
+++ dot-net/ReliableDownloader.Tests/Tests.cs
@@ -1,17 +1,51 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using System.Net;
 
 namespace ReliableDownloader.Tests;
 
 [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
 public class Tests
 {
-    private readonly FileDownloader _sut = new();
+    private readonly FileDownloader _sut;
+    private readonly Mock<IWebSystemCalls> _mockWebCalls = new();
+    private FileProgress? _lastProgress;
+    private CancellationTokenSource _cts;
+
+    public Tests()
+    {
+        _sut = new FileDownloader(_mockWebCalls.Object);
+        _cts = new CancellationTokenSource();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _cts.Dispose();
+    }
 
     [Test]
-    public async Task Test1()
+    public async Task TryDownloadFile_ShouldReturnFalse_WhenHeadersRequestFails()
     {
-        await _sut.TryDownloadFile("https://example.com/example.msi", "example.msi", _ => { }, default);
+        // Arrange
+        var url = "http://fail.com/file.msi";
+        var filePath = "file.msi";
+        var response = new HttpResponseMessage(HttpStatusCode.NotFound); // Simulate failure
 
-        Assert.Inconclusive("TODO");
+        _mockWebCalls.Setup(w => w.GetHeadersAsync(url, _cts.Token))
+                     .ReturnsAsync(response);
+
+        // Act
+        var result = await _sut.TryDownloadFile(url, filePath, HandleProgress, _cts.Token);
+
+        // Assert
+        Assert.That(result == false);
+        _mockWebCalls.Verify(w => w.GetHeadersAsync(url, _cts.Token), Times.Once); // Verify HEAD call was made
+    }
+
+    private void HandleProgress(FileProgress progress)
+    {
+        _lastProgress = progress;
+        Console.WriteLine($"Progress: {progress}");
     }
 }
